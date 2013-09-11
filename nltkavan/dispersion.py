61f9380610a26002d3bace037533419ecd43790d
Stéphanie Vilayphiou <stephanie@stdin.fr>
2013-09-11 16:40:52 +0200

    on prend en compte les pronoms dans le graphe des noms

--- nltkavan/dispersion.py
+++ nltkavan/dispersion.py
@@ -10,11 +10,11 @@ tokens = nltk.word_tokenize(raw)
 text = nltk.Text(tokens)
 
 ice_locations = ["Atlantic", "Britain", "Cologne", "France", "United", "States"]
-ice_names = ["Anna", "Dennis", "Dick", "Helen", "John", "Jorge", "K.", "Luis", "M.", "Owen", "Paul", "Peter", "Philip", "Potter", "S.", "Stone", "Thomas", "William"]
+ice_names = ["Anna", "Dennis", "Dick", "Helen", "John", "Jorge", "K.", "Luis", "M.", "Owen", "Paul", "Peter", "Philip", "Potter", "S.", "Stone", "Thomas", "William", "I", "you", "he", "she", "we", "they"]
 
 cat_locations = ["China", "Cuba", "England", "Florida", "France", "Germany", "Hollywood", "Hong", "Kong", "India", "Indianapolis", "Japan", "London", "Manhattan", "Newport", "Newton", "N.Y.", "New", "York", "Puerto", "Rhode", "Russia", "San", "Lorenzo", "Santa", "Tokyo"]
 
-cat_names = ["Albert", "Schweitzer", "Barbara", "Hutton", "Betsy", "Ross", "Lionel", "Boyd", "Harrison", "C.", "Conners", "Castle", "Cedar", "Charles", "Atlas", "David", "McCabe", "Felix", "Frank", "Franklin", "H.", "Lowe", "Crosby", "Harry", "Truman", "I", "Jack", "Johnson","Julian", "Castle", "Lewis", "Marvin", "Breed", "Faust", "Pefko", "Fata", "Morgana", "Newton", "Hoenikker", "Philip", "Robinson", "Sandra", "Stanley"]
+cat_names = ["Albert", "Schweitzer", "Barbara", "Hutton", "Betsy", "Ross", "Lionel", "Boyd", "Harrison", "C.", "Conners", "Castle", "Cedar", "Charles", "Atlas", "David", "McCabe", "Felix", "Frank", "Franklin", "H.", "Lowe", "Crosby", "Harry", "Truman", "Jack", "Johnson","Julian", "Castle", "Lewis", "Marvin", "Breed", "Faust", "Pefko", "Fata", "Morgana", "Newton", "Hoenikker", "Philip", "Robinson", "Sandra", "Stanley", "I", "you", "he", "she", "we", "they"]
 
 
-text.dispersion_plot(cat_locations)
+text.dispersion_plot(cat_names)
