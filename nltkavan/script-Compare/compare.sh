13664c27b1f12d1b44e0f166a7fdd1ae684a73dd
cath <cath@cath-1001PXD.(none)>
2014-04-17 13:48:35 +0200

    création dossier avec les résultats des comparaisons de paires d'oeuvres Let Me Alone/Stranger Still et who Are You/Ice

---
+++ nltkavan/script-Compare/compare.sh
@@ -0,0 +1,16 @@
+#! /usr/bin/env bash
+
+# USAGE
+#
+# Outputs a 3 column list of words: 1 = words only in file1; 2 = words only in file2; 3 = words in both texts
+# ./compare.sh /path/to/file1.txt /path/to/file2.txt
+#
+# Outputs a list of common words of two texts
+# ./compare.sh /path/to/file1.txt /path/to/file2.txt common 
+
+if [ "$3" = "common" ]; 
+    then
+       comm -12 $1 $2
+    else
+        comm $1 $2
+fi
